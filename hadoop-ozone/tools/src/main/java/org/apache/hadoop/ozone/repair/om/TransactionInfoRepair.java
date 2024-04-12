d9d4fcb9152989089224ae79a3aa2d1811d5f1bf
DaveTeng0 <dteng@cloudera.com>
2024-04-12 10:00:43 -0700

    add apache license on TransactionInfoRepair

--- hadoop-ozone/tools/src/main/java/org/apache/hadoop/ozone/repair/om/TransactionInfoRepair.java
+++ hadoop-ozone/tools/src/main/java/org/apache/hadoop/ozone/repair/om/TransactionInfoRepair.java
@@ -1,3 +1,25 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license
+ * agreements. See the NOTICE file distributed with this work for additional
+ * information regarding
+ * copyright ownership. The ASF licenses this file to you under the Apache
+ * License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License. You may obtain a
+ * copy of the License at
+ *
+ * <p>http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * <p>Unless required by applicable law or agreed to in writing, software
+ * distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and
+ * limitations under the License.
+ */
+
 package org.apache.hadoop.ozone.repair.om;
 
 
@@ -30,12 +52,6 @@ import static org.apache.hadoop.ozone.OzoneConsts.TRANSACTION_INFO_KEY;
 import static org.apache.hadoop.ozone.om.OmMetadataManagerImpl.TRANSACTION_INFO_TABLE;
 
 
-//@CommandLine.Command(
-//    name = "cancelprepare",
-//    description = "Cancel prepare state in the OMs.",
-//    mixinStandardHelpOptions = true,
-//    versionProvider = HddsVersionProvider.class
-//)
 
 /**
  * Tool to update the highest index in transactionInfoTable.
@@ -45,10 +61,8 @@ import static org.apache.hadoop.ozone.om.OmMetadataManagerImpl.TRANSACTION_INFO_
     description = "CLI to update the highest index in transactionInfoTable.",
     mixinStandardHelpOptions = true,
     versionProvider = HddsVersionProvider.class
-
 )
 @MetaInfServices(SubcommandWithParent.class)
-
 public class TransactionInfoRepair implements Callable<Void>, SubcommandWithParent  {
 
 //  @CommandLine.ParentCommand
